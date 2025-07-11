6c9de8370f0fdc5a13de1312bdbedaceef668f92
Hemraj Soyal <hemrajsoyal10@gmail.com>
2025-07-11 14:56:37 +0530

    LTS-1 Setup Authentication

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:leetsave/presentation/auth/pages/sign_in.dart' show SignIn;
+import 'package:leetsave/presentation/auth/pages/signed_in.dart';
+import 'package:leetsave/presentation/splash_screen/pages/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,9 +10,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Leetsave',
-      theme: ThemeData.dark(useMaterial3: true),
-      // home: const LoginPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        '/': (context) => SignIn(),
+        '/signed_in': (context) => const SignedIn()
+      }
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:leetsave/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  // final url = dotenv.env['SUPABASE_URL'] ?? '';
+  // final anonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+  final url = "https://bzlhkenptoipqgjhjrdo.supabase.co";
+  final anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6bGhrZW5wdG9pcHFnamhqcmRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTkxMDAsImV4cCI6MjA2NzQ3NTEwMH0.6ddHzbmjFdPYznn2UUf6jath9gHxYceLHrrXmr7yr_o";
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  await Supabase.initialize(url: url, anonKey: anonKey);
+  // await dotenv.load();
 
 
   runApp(const MyApp());
